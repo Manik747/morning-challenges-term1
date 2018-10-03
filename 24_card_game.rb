eab903c21b37465bf8443338ed53ebaa7dcdf69b
Matthew Martin <m.martin747@gmail.com>
2018-10-04 09:49:50 +1000

    sync master

--- 24_card_game.rb
+++ 24_card_game.rb
@@ -1,10 +1,19 @@
 class Card
     def initialize(suit, rank)
-        # Create a new card
+        @suit=suit
+        @rank=rank
     end
     
     def face_card?
         # Is the card a face card (above 10)?
+        if rank == 11
+            rank = "Jack"
+        elsif rank == 12
+            rank = "Queen"
+        else rank == 13
+            rank = "King"
+        end
+            
     end 
     
     def to_s
